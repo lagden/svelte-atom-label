ecb11a8f95e3160ce6b2cc7b05e4cf0b0bfde7b9
Thiago Lagden <lagden@gmail.com>
2021-05-18 15:39:35 -0300

    fix props

--- src/Label.svelte
+++ src/Label.svelte
@@ -1,12 +1,12 @@
 <script>
-	// Props class
-	let className = ''
-	export {className as class}
+const restPropsClass = String($$restProps?.class ?? '').split(' ')
+$$restProps.class = [
+	...new Set(['_atom_frm__label', ...restPropsClass])
+].join(' ')
 </script>
 
 <label
-	class="_atom_frm__label {className}"
-	class:_atom_frm__label___disabled={$$restProps.disabled}
-	class:_atom_frm__label___required={$$restProps.required}
+	class:_atom_frm__label___disabled={$$restProps?.disabled ?? false}
+	class:_atom_frm__label___required={$$restProps?.required ?? false}
 	{...$$restProps}
 ><slot /></label>
